91ec9563a78ca42ba6ceec0c49536500d41f617e
Jan Hentschel <jan.hentschel@plusforta.de>
2022-01-17 09:21:33 +0100

    Refs PF-8318: ERP - kautionsfrei Produkte für 4,7 und 5,25% ermöglichen
    added insurance condition for kfde products

--- src/Type/VersicherungsbedingungenEnumTyp.php
+++ src/Type/VersicherungsbedingungenEnumTyp.php
@@ -8,20 +8,24 @@ use Webmozart\Assert\Assert;
 
 class VersicherungsbedingungenEnumTyp
 {
-    const MIET_2018 = 'Miet2018';
-    const MIET_2019_AB = 'Miet19AB';
+    public const MIET_2018 = 'Miet2018';
+    public const MIET_19_AB = 'Miet19AB';
+    public const MIET_2019_AB = 'Miet2019AB';
+    public const MIET_0819 = 'Miet0819';
+
 
     /**
      * @var string
      */
-    private $Versicherungsbedingungen;
+    private string $Versicherungsbedingungen;
 
     public function withVersicherungsbedingungen($bedingung = self::MIET_2018): VersicherungsbedingungenEnumTyp
     {
         Assert::oneOf($bedingung, [
             self::MIET_2018,
+            self::MIET_19_AB,
             self::MIET_2019_AB,
-            'Miet2019AB',
+            self::MIET_0819,
         ]);
         $new = clone $this;
         $new->Versicherungsbedingungen = $bedingung;
