a39bd3c4a4d9c535dbc8c6326853ce2cd59dfd7b
Jan Hentschel <jan.hentschel@plusforta.de>
2022-02-17 16:05:19 +0100

    Refs PF-8407: ERP - R+V Schnittstelle 2020_2 führt zu Fehler bei zwei Mietern
    fixed Versicherungsnehmertyp

--- src/Type/VersicherungsnehmerTyp.php
+++ src/Type/VersicherungsnehmerTyp.php
@@ -2,18 +2,131 @@
 
 namespace PlusForta\RuVSoapBundle\Type;
 
-/**
- * @property NatuerlichePersonErweitertTyp NatuerlichePerson
- * @property \SoapVar<GemeinschaftTyp> Gemeinschaft
- */
 class VersicherungsnehmerTyp
 {
+    /**
+     * @var \PlusForta\RuVSoapBundle\Type\NameTyp
+     */
+    private $Name;
 
     /**
-     * @param NatuerlichePersonErweitertTyp $NatuerlichePerson
+     * @var string
+     */
+    private $Strasse;
+
+    /**
+     * @var string
+     */
+    private $Postleitzahl;
+
+    /**
+     * @var string
+     */
+    private $Ort;
+
+    /**
+     * @var \PlusForta\RuVSoapBundle\Type\NatuerlichePersonErweitertTyp
+     */
+    private $NatuerlichePerson;
+
+    /**
+     * @var \PlusForta\RuVSoapBundle\Type\GemeinschaftTyp
+     */
+    private $Gemeinschaft;
+
+    /**
+     * @return \PlusForta\RuVSoapBundle\Type\NameTyp
+     */
+    public function getName()
+    {
+        return $this->Name;
+    }
+
+    /**
+     * @param \PlusForta\RuVSoapBundle\Type\NameTyp $Name
      * @return VersicherungsnehmerTyp
      */
-    public function withNatuerlichePerson(NatuerlichePersonErweitertTyp $NatuerlichePerson): VersicherungsnehmerTyp
+    public function withName($Name)
+    {
+        $new = clone $this;
+        $new->Name = $Name;
+
+        return $new;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStrasse()
+    {
+        return $this->Strasse;
+    }
+
+    /**
+     * @param string $Strasse
+     * @return VersicherungsnehmerTyp
+     */
+    public function withStrasse($Strasse)
+    {
+        $new = clone $this;
+        $new->Strasse = $Strasse;
+
+        return $new;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostleitzahl()
+    {
+        return $this->Postleitzahl;
+    }
+
+    /**
+     * @param string $Postleitzahl
+     * @return VersicherungsnehmerTyp
+     */
+    public function withPostleitzahl($Postleitzahl)
+    {
+        $new = clone $this;
+        $new->Postleitzahl = $Postleitzahl;
+
+        return $new;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrt()
+    {
+        return $this->Ort;
+    }
+
+    /**
+     * @param string $Ort
+     * @return VersicherungsnehmerTyp
+     */
+    public function withOrt($Ort)
+    {
+        $new = clone $this;
+        $new->Ort = $Ort;
+
+        return $new;
+    }
+
+    /**
+     * @return \PlusForta\RuVSoapBundle\Type\NatuerlichePersonErweitertTyp
+     */
+    public function getNatuerlichePerson()
+    {
+        return $this->NatuerlichePerson;
+    }
+
+    /**
+     * @param \PlusForta\RuVSoapBundle\Type\NatuerlichePersonErweitertTyp $NatuerlichePerson
+     * @return VersicherungsnehmerTyp
+     */
+    public function withNatuerlichePerson($NatuerlichePerson)
     {
         $new = clone $this;
         $new->NatuerlichePerson = $NatuerlichePerson;
@@ -22,17 +135,23 @@ class VersicherungsnehmerTyp
     }
 
     /**
-     * @param GemeinschaftTyp $Gemeinschaft
+     * @return \PlusForta\RuVSoapBundle\Type\GemeinschaftTyp
+     */
+    public function getGemeinschaft()
+    {
+        return $this->Gemeinschaft;
+    }
+
+    /**
+     * @param \PlusForta\RuVSoapBundle\Type\GemeinschaftTyp $Gemeinschaft
      * @return VersicherungsnehmerTyp
      */
-    public function withGemeinschaft(GemeinschaftTyp $Gemeinschaft): VersicherungsnehmerTyp
+    public function withGemeinschaft($Gemeinschaft)
     {
         $new = clone $this;
-        $new->Gemeinschaft = $Gemeinschaft->toSoapVar();
+        $new->Gemeinschaft = $Gemeinschaft;
 
         return $new;
     }
-
-
 }
 
