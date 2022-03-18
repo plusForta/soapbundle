0c221c98107d92e57ba387e4da160d423358d866
Jan Hentschel <jan.hentschel@plusforta.de>
2022-03-18 08:47:10 +0100

    Update versions for php8 compatibility

--- src/DependencyInjection/PlusFortaRuVSoapExtension.php
+++ src/DependencyInjection/PlusFortaRuVSoapExtension.php
@@ -66,7 +66,7 @@ class PlusFortaRuVSoapExtension extends Extension
 
     }
 
-    public function getAlias()
+    public function getAlias(): string
     {
         return 'plusforta_ruv_soap';
     }
