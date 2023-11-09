9c2b395a003e7310d50fa05af36ed277b18053fe
Marc Runkel <marc.runkel@plusforta.de>
2023-11-09 11:28:10 +0100

    Remove useless assert

--- src/Type/VersicherungsbedingungenEnumTyp.php
+++ src/Type/VersicherungsbedingungenEnumTyp.php
@@ -8,25 +8,10 @@ use Webmozart\Assert\Assert;
 
 class VersicherungsbedingungenEnumTyp
 {
-    public const MIET_2018 = 'Miet2018';
-    public const MIET_19_AB = 'Miet19AB';
-    public const MIET_2019_AB = 'Miet2019AB';
-    public const MIET_0819 = 'Miet0819';
-
-
-    /**
-     * @var string
-     */
     private string $Versicherungsbedingungen;
 
-    public function withVersicherungsbedingungen($bedingung = self::MIET_2018): VersicherungsbedingungenEnumTyp
+    public function withVersicherungsbedingungen($bedingung = 'MIET0422'): VersicherungsbedingungenEnumTyp
     {
-        Assert::oneOf($bedingung, [
-            self::MIET_2018,
-            self::MIET_19_AB,
-            self::MIET_2019_AB,
-            self::MIET_0819,
-        ]);
         $new = clone $this;
         $new->Versicherungsbedingungen = $bedingung;
 
